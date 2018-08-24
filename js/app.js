89ba59caa43c4709412f6b6f031cf278928d1707
Andrii Suprun <smiranin007@gmail.com>
2018-08-24 10:05:59 +0200

    init project

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+const page = new PageList({
+  el: document.getElementById('root')
+});
+
+const pagePhone = new PagePhone({
+  el: document.getElementById('root'),
+  tmpl: document.getElementById('page-phone-tmpl'),
+  id: 'motorola-xoom-with-wi-fi'
+});
---
+++ js/components/page-phone.js
@@ -0,0 +1,15 @@
+class PagePhone {
+
+  constructor(options) {
+    this.el = options.el;
+    this.tmpl = _.template(options.tmpl.innerHTML);
+    this.id = options.id;
+
+    this.init();
+  }
+
+  init() {
+    const phone = phones.find((phoneItem) => phoneItem.id === this.id);
+    this.el.innerHTML = this.tmpl(phone);
+  }
+}
